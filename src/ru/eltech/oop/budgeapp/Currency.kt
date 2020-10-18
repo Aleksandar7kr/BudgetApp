311e2ede324c31a7e9b28bb16915f0825e00b2b0
Aleksandr Krasilnikov <aleksandar7kr@gmail.com>
2020-10-18 22:46:52 +0300

    convert Currency to Kotlin

--- src/ru/eltech/oop/budgeapp/Currency.kt
+++ src/ru/eltech/oop/budgeapp/Currency.kt
@@ -1,20 +1,11 @@
-package ru.eltech.oop.budgeapp;
+package ru.eltech.oop.budgeapp
 
-public enum Currency {
+enum class Currency(val symbol: String) {
     RUB("₽"),
-
     USD("$"),
-
     EUR("€");
 
-    private final String symbol;
-
-    Currency(String symbol) {
-        this.symbol = symbol;
-    }
-
-    @Override
-    public String toString() {
-        return symbol;
+    override fun toString(): String {
+        return symbol
     }
 }
